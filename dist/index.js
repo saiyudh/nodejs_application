016b3f39cd7fa0a9421235855d32b2aa0a062522
Saiyudh Mannan <saiyudh.mannan@gmail.com>
2022-09-09 18:31:12 +0200

    adding the git secrets file

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,16 @@
 var __webpack_exports__ = {};
 async function run(){
     console.log('Hello,World!');
+    const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+    const octokit = core.getOctokit(GITHUB_TOKEN);
+    const { context = {} } = github;
+    const { pull_request } = context.payload;
+    await octokit.rest.issues.createComment({
+        ...context.repo,
+        issue_number: pull_request.number,
+        body: "Thank you for submitting we will try to reach you",
+      });
+    
 }
 
 run();
--- src/action.js
+++ src/action.js
@@ -1,5 +1,15 @@
 async function run(){
     console.log('Hello,World!');
+    const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+    const octokit = core.getOctokit(GITHUB_TOKEN);
+    const { context = {} } = github;
+    const { pull_request } = context.payload;
+    await octokit.rest.issues.createComment({
+        ...context.repo,
+        issue_number: pull_request.number,
+        body: "Thank you for submitting we will try to reach you",
+      });
+    
 }
 
 run();
